a328b2d0c96bc158ce526a957c99e6b3cdd6eadd
Diego Martinez <diego.martinez.r@tecsup.edu.pe>
2018-07-04 02:50:52 -0500

    ds

--- backend/modelos/area.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var EsquemaArea = mongoose.Schema({
-	Nombre : String,
-	Abreviatura: String,
-	Estado : String
-});
-
-module.exports = mongoose.model( "Areas", EsquemaArea ,'Areas');
